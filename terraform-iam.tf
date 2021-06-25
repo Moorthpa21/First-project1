d0b48e35bf4dc739563e7d28e896a2d455615e9c
Moorthpa21 <77835752+Moorthpa21@users.noreply.github.com>
2021-06-25 16:03:17 +0530

    Add files via upload

---
+++ terraform-iam.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+resource "aws_iam_user" "user" {
+  name = "Parimala"
+}
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.lambda_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:*",
+          "lambda:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+ }
